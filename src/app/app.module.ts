bd33f905948325516fe53b8b87f80332f9e95014
Galen Gong <galengong@gmail.com>
2025-03-02 16:02:20 -0500

    Hook up mongodb and add user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from '../user/user.module';
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/mydatabase'), // Replace with your DB name
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
